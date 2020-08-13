62da6445d5039f5565a6c18f868777c706374a0c
michaelj916 <46703736+michaelj916@users.noreply.github.com>
2020-08-13 11:47:35 -0700

    Updates

---
+++ precache-manifest.9fb8b38aeca4abecfd67fb39a1180335.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e6b9461bf190ac8ed9a65e036f9da6",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "9efbb0b59042da0a2e2b",
+    "url": "/my-react-portfolio/static/css/main.843b04d0.chunk.css"
+  },
+  {
+    "revision": "9ca66757fb353e86615d",
+    "url": "/my-react-portfolio/static/js/2.dd54bc13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-portfolio/static/js/2.dd54bc13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9efbb0b59042da0a2e2b",
+    "url": "/my-react-portfolio/static/js/main.b56cc1d4.chunk.js"
+  },
+  {
+    "revision": "a09521d6aa79f251d523",
+    "url": "/my-react-portfolio/static/js/runtime-main.bdbff3dd.js"
+  },
+  {
+    "revision": "b6bf73373991fcca0696da05c0c59f89",
+    "url": "/my-react-portfolio/static/media/noise.b6bf7337.png"
+  }
+]);
